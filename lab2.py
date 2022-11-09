81faa0c52f951cedecbcc1c4626bd1147b674934
Tirthkumar Patel <114356041+myseneca-147695209@users.noreply.github.com>
2022-11-09 14:20:11 -0500

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+name = sys.argv[1]
+age = sys.argv[2]
+ 
+print('Hi ' + name + '. you are ' + age + ' years old.')
+print('variable1 = ' + name + ' ' )
+print('variable2 = ' + age + ' ' )
+print('The script and variables used are: ' , str(sys.argv))
+
